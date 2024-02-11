20e62fb8e4c5441bb3eafa843717831c1dfaec19
ansnekit <ansnekit@gmial.com>
2024-02-11 20:01:13 +0300

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/clone-youtube-vue3/',
+    base: '/',
     plugins: [vue()],
     resolve: {
         alias: {
